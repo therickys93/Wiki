eca70ec30d5247fd69cb089f0c3f20be8082edf8
Riccardo Crippa <therickys93@gmail.com>
2018-03-31 15:31:10 +0200

    aggiunto redis commander in docker compose

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ docker build -t therickys93/wikiredis wikiredis/
 docker build -t therickys93/wikicontroller wikicontroller/
 docker build -t therickys93/wikiproxy wikiproxy/
 docker build -t therickys93/wikimediaserver wikimediaserver/
+docker build -t therickys93/wikirediscommander wikirediscommander/
