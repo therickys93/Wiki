3a907b79fd18bdd94c1460ade1cee10c3484e3e7
Riccardo Crippa <therickys93@gmail.com>
2018-04-01 13:36:54 +0200

    aggiunta immagine wiki server in docker compose

--- build.sh
+++ build.sh
@@ -5,3 +5,4 @@ docker build -t therickys93/wikicontroller wikicontroller/
 docker build -t therickys93/wikiproxy wikiproxy/
 docker build -t therickys93/wikimediaserver wikimediaserver/
 docker build -t therickys93/wikirediscommander wikirediscommander/
+docker build -t therickys93/wikiserver wikiserver/
