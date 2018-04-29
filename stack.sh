83a38f9abf9536439150bd49e6367970f3d9c8d9
Riccardo Crippa <therickys93@gmail.com>
2018-04-29 10:38:25 +0200

    prima implementazione dello stack

---
+++ stack.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# bisogna essere nel node master di uno swarm per far andare questi comandi
+# docker swarm init
+docker service create --name registry --publish published=5000,target=5000 registry:2
+docker-compose build
+docker-compose up -d
+docker-compose ls
+docker-compose down --volumes
+docker-compose push 
+docker stack deploy --compose-file docker-compose.yml wiki
