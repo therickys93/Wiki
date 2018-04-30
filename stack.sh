f0e2cf334bd5c2c591d291e21a6646ab52931317
Riccardo Crippa <therickys93@gmail.com>
2018-04-30 10:17:37 +0200

    cambiamento su un file stack

--- stack.sh
+++ stack.sh
@@ -3,9 +3,9 @@
 # bisogna essere nel node master di uno swarm per far andare questi comandi
 # docker swarm init
 docker service create --name registry --publish published=5000,target=5000 registry:2
-docker-compose build
-docker-compose up -d
-docker-compose ps
-docker-compose down --volumes
-docker-compose push 
+docker-compose -f stack.yml build
+docker-compose -f stack.yml up -d
+docker-compose -f stack.yml ps
+docker-compose -f stack.yml down --volumes
+docker-compose -f stack.yml push 
 docker stack deploy --compose-file stack.yml --with-registry-auth wiki
